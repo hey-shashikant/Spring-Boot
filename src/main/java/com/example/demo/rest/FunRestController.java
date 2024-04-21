38f686d0d1b443c2842b75ae6c84a9e0c1b9c412
Shashikant Solanki <90645693+hey-shashikant@users.noreply.github.com>
2024-04-22 01:05:29 +0530

    added some more endpoints

--- src/main/java/com/example/demo/rest/FunRestController.java
+++ src/main/java/com/example/demo/rest/FunRestController.java
@@ -11,4 +11,19 @@ public class FunRestController {
     public String sayHello() {
         return "Hello World!";
     }
+
+    // expose a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5K!";
+    }
+
+    // expose a new endpoint for "fortune"
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is my lucky day!";
+    }
+
 }
